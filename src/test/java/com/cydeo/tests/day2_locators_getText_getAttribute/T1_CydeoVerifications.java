117fb46df49cc6c156f44844cb25ebb8ead1bd8e
Talha Ozbabuccu <talhaoz105@gmail.com>
2022-12-25 17:43:38 +0300

    day2 Task1_CydeoVerifications

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -1,6 +1,8 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T1_CydeoVerifications {
     public static void main(String[] args) {
@@ -8,8 +10,12 @@ public class T1_CydeoVerifications {
         //TC    #1: Cydeo practice tool verifications
         //1. Open chrome brewser
         WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+
 
         //2. Go to https://paractice.cydeo.com
+
         //3. Verify URL contains
         //Expected: cydeo
         //4. Verify title:
