978a84288c78dabb66602d8631c1d183c432f4d3
Talha Ozbabuccu <talhaoz105@gmail.com>
2022-12-25 17:39:40 +0300

    day2 Task1_CydeoVerifications

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -0,0 +1,21 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class T1_CydeoVerifications {
+    public static void main(String[] args) {
+
+        //TC    #1: Cydeo practice tool verifications
+        //1. Open chrome brewser
+        WebDriverManager.chromedriver().setup();
+
+        //2. Go to https://paractice.cydeo.com
+        //3. Verify URL contains
+        //Expected: cydeo
+        //4. Verify title:
+        //Expected: Practice
+
+
+
+    }
+}
