ff3138d7a4ace1cab80d11157d4c3f0055b7befd
Talha Özbabuccu <talhaoz105@gmail.com>
2023-04-17 03:52:30 +0300

    day4 Task1 xpath_cssSelector_practice

---
+++ src/test/java/com/cydeo/tests/day3_cssSelektor_xpath/T2_getText_getAttribute.java
@@ -0,0 +1,59 @@
+package com.cydeo.tests.day3_cssSelektor_xpath;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T2_getText_getAttribute {
+
+    public static void main(String[] args) {
+
+        //TC #2: NextBaseCRM, locators, getText(), getAttribute() practice
+        //1- Open a Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2- Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+        //3- Verify "remember me" label text is as expected:
+        //Expected: Remember me on this computer
+        WebElement rememberLabel = driver.findElement(By.className("login-item-checkbox-label"));
+
+        String expectedRememberMeLabel = "Remember me on this computer";
+        String actualRememberMeLabel = rememberLabel.getText();
+
+        if (actualRememberMeLabel.equalsIgnoreCase(expectedRememberMeLabel)){
+            System.out.println("Label verification PASSED!");
+        }else{
+            System.out.println("Label verification FAILED!!!");
+        }
+
+        //4- Verify "forgot password" link text is as expected:
+        //Expected: Forgot your password?
+        WebElement forgotPasswordLink = driver.findElement(By.className("login-link-forgot-pass"));
+
+        String expectedForgotPasswordLinkText = "Forgot your password?";
+        String actualForgotPasswordLinkText = forgotPasswordLink.getText();
+
+        if (actualForgotPasswordLinkText.equalsIgnoreCase(expectedForgotPasswordLinkText)){
+            System.out.println("Forgot password link verification PASSED!");
+        }else{
+            System.out.println("Forgot password link verification FAILED!!!");
+        }
+
+    //5- Verify "forgot password" href attribute`s value contains expected:
+        //Expected: forgot_password=yes
+
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
@@ -0,0 +1,32 @@
+package com.cydeo.tests.day4_findElements_checkboxes_radio;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class T1_xpath_cssSelector_practice {
+    public static void main(String[] args) {
+        //XPATH and CSS Selector PRACTICES
+        //DO NOT USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
+        //TC #1: XPATH and cssSelector Practices
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+
+        //2. Go to https://practice.cydeo.com/forgot_password
+        driver.get("https://practice.cydeo.com/forgot_password");
+
+        //3. Locate all the WebElements on the page using XPATH and/or CSS
+        //locator only (total of 6)
+        //a. “Home” link
+        //b. “Forgot password” header
+        //c. “E-mail” text
+        //d. E-mail input box
+        //e. “Retrieve password” button
+        //f. “Powered by Cydeo text
+        //4. Verify all web elements are displayed.
+    }
+
+
+
+}
