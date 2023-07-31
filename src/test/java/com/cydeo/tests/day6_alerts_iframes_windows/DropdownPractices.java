8d2e862e463dbfe7f1a965edea3f08d35c51a921
Talha Özbabuccu <talhaoz105@gmail.com>
2023-07-31 12:24:14 +0300

    day6  T1 ALERT Practice 1

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -39,6 +40,40 @@ public class DropdownPractices {
         String actualOptionText= stateDropdown.getFirstSelectedOption().getText();
 
         Assert.assertEquals(actualOptionText,expectedOptionText);
-
     }
+
+    @Test
+    public void dropdown_task6(){
+        //Select "DEcember1 , 1923" and verify it's selected. Select year using : visible text
+        Select yearDropdown = new Select(driver.findElement(By.xpath("//select[@id='year']")));
+        Select monthDropdown = new Select(driver.findElement(By.xpath("//select[@id='month']")));
+        Select dayDropdown = new Select(driver.findElement(By.xpath("//select[@id='day']")));
+
+        //Select year using: visible text
+        yearDropdown.selectByVisibleText("1924");
+        //Select month using : value attribute
+        monthDropdown.selectByValue("11");
+        //Select day using : index number
+        dayDropdown.selectByIndex(0);
+
+        //created expected values
+        String expectedYear= "1924";
+        String expectedMonth= "December";
+        String expectedDay= "1";
+
+        //getting actual values from browser
+        String actualYear= yearDropdown.getFirstSelectedOption().getText();
+        String actualMonth= monthDropdown.getFirstSelectedOption().getText();
+        String actualDay= dayDropdown.getFirstSelectedOption().getText();
+
+        //create assertions
+        Assert.assertTrue(actualYear.equals(expectedYear));
+        Assert.assertEquals(actualMonth,expectedMonth);
+        Assert.assertEquals(actualDay,expectedDay);
+     }
+
+     @AfterMethod
+    public void teardownMethod(){
+         driver.close();
+     }
 }
---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
@@ -0,0 +1,28 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_Alert_Practices {
+
+    public WebDriver driver;
+    @BeforeMethod
+    public void setupMethod(){
+        //2. Go to https://practice.cydeo.com/dropdown
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/dropdown");
+    }
+
+    @Test
+    public void alert_test1(){
+
+    }
+
+
+}
