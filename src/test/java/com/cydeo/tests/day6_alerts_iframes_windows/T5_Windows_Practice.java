6072dcb074c12d2417dab48e961cc5fccef03901
Talha Özbabuccu <talhaoz105@gmail.com>
2023-08-01 14:23:05 +0300

    day6  T5 window task

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class T5_Windows_Practice {
@@ -39,11 +41,27 @@ public class T5_Windows_Practice {
         WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
         clickHereLink.click();
 
-        actualTitle= driver.getTitle();
-        System.out.println("Title after click: " + actualTitle);
-
         //Switch to new Window.
+        Set<String> allWindowHandles =driver.getWindowHandles();
+
+        for(String each: allWindowHandles){
+            driver.switchTo().window(each);
+            System.out.println("Current title while switching windows: "+ driver.getTitle());
+        }
+
         //Assert: Title is "New Window"
+        String expectedTitleAfterClick = "New Window";
+        actualTitle= driver.getTitle();
+
+        Assert.assertEquals(actualTitle,expectedTitleAfterClick);
+
+        System.out.println("Title after click: " + actualTitle);
+    }
+
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
     }
 
 }
