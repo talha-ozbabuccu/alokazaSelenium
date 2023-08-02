44049ba1860e40f8af11053a88fc7e6e788cb449
Talha Özbabuccu <talhaoz105@gmail.com>
2023-08-02 13:07:58 +0300

    day7  T1 window handling task

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -1,10 +1,13 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class T1_WindowHandling {
@@ -18,13 +21,29 @@ public class T1_WindowHandling {
     @Test
     public void window_handling_test(){
         //Go to : https://www.amazon.com
+        driver.get("https://www.amazon.com");
 
-        //Copy paste the lines from below into your class
+        //Copy and paste the lines from below into your class
+        ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
+
         //Create a logic to switch to the tab where Etsy.com is open
+        Set<String >allWindowHandles=driver.getWindowHandles();
+        for (String each : allWindowHandles) {
+            driver.switchTo().window(each);
+            System.out.println("Current URL: "+driver.getCurrentUrl());
+            if (driver.getCurrentUrl().contains("etsy")){
+                break;
+            }
+        }
         //Assert: Title contains "Etsy"
-        //Lines to be pasted:
-        //((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');")
+        String actualTitle=driver.getTitle();
+        String expectedInTitle= "Etsy";
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
 
+        //Assert.assertTrue(driver.getTitle().contains("Etsy"));
+        //Lines to be pasted:
 
     }
 
