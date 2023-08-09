d329ab147022c9c65f6157de3d83abc78a29a25e
Talha Özbabuccu <talhaoz105@gmail.com>
2023-08-09 13:10:23 +0300

    day9 JavaFakerPractice

---
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
@@ -0,0 +1,14 @@
+package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
+
+import org.testng.annotations.Test;
+
+public class JavaFakerPractice {
+
+    @Test
+    public void test1(){
+
+
+    }
+
+
+}
