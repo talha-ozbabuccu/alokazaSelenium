e6f46cdc99d80423213c4fd3f524bf83a2b1915e
Talha Özbabuccu <talhaoz105@gmail.com>
2023-08-11 18:22:47 +0300

    day9 SingletonPractice.java

---
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/SingletonPractice.java
@@ -0,0 +1,11 @@
+package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
+
+import org.testng.annotations.Test;
+
+public class SingletonPractice {
+
+    @Test
+    public void singleton_understand_test1(){
+
+    }
+}
