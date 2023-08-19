18ffa617a0db8f95e9ff2a2abb20ec61942c2c28
Talha Özbabuccu <talhaoz105@gmail.com>
2023-08-19 19:39:05 +0300

    day11 T7_Scroll_JSExecutor.java

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T7_Scroll_JSExecutor.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T7_Scroll_JSExecutor.java
@@ -1,6 +1,8 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T7_Scroll_JSExecutor {
@@ -13,7 +15,11 @@ public class T7_Scroll_JSExecutor {
         Driver.getDriver().get("https://practice.cydeo.com/large");
 
         //3- Scroll down to “Cydeo” link
+        WebElement cydeoLink= Driver.getDriver().findElement(By.linkText("CYDEO"));
+        WebElement homeLink= Driver.getDriver().findElement(By.linkText("Home"));
+
         //4- Scroll up to “Home” link
+
         //5- Use below provided JS method only
         //JavaScript method to use : arguments[0].scrollIntoView(true)
         //Note: You need to locate the links as web elements and pass them as
