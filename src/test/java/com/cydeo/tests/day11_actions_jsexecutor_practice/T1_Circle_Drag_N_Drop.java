fde2b9e6514e72c69bad665d6f01808c928e0463
Talha Özbabuccu <talhaoz105@gmail.com>
2023-08-28 23:58:26 +0300

    day12

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
@@ -1,8 +1,11 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T1_Circle_Drag_N_Drop {
@@ -16,13 +19,31 @@ public class T1_Circle_Drag_N_Drop {
         WebElement acceptCookiesButton = Driver.getDriver().findElement(By.id("onetrust-accept-btn-handler"));
         acceptCookiesButton.click();
 
-        //2. Drag and drop the small circle to bigger circle.
+        //Locate small and big circles to be able to drag them around
         WebElement smallCircle = Driver.getDriver().findElement(By.id("draggable"));
         WebElement bigCircle = Driver.getDriver().findElement(By.id("droptarget"));
 
+        // Use below JavaScript method and scroll
+        //JavascriptExecutor js= (JavascriptExecutor) Driver.getDriver();
+        BrowserUtils.sleep(1);
+        //js.executeScript("window.scrollBy(0,750)");
+
+        //2. Drag and drop the small circle to bigger circle.
+        Actions actions= new Actions(Driver.getDriver());
+        //actions.dragAndDrop(smallCircle,bigCircle).perform();
+        actions.clickAndHold(smallCircle)
+        .pause(2000)
+                .moveToElement(bigCircle)
+                .pause(2000).release().perform();
 
         //3. Assert:
         //-Text in big circle changed to: “You did great!”
+
+        String actualBigCircleText = bigCircle.getText();
+        String expectedBigCircleText = "You did great!";
+
+        Assert.assertEquals(actualBigCircleText,expectedBigCircleText);
+
     }
 
 }
