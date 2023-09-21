9ee5db2549a36b863d12d5d0a50a9de7a8d0dba7
Talha Özbabuccu <talhaoz105@gmail.com>
2023-09-21 15:32:23 +0300

    day12 LibraryLoginPage.java

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day12_pom_design_explicit_wait;
 
+import com.cydeo.pages.LibraryLoginPage;
 import com.cydeo.utilities.Driver;
 import org.testng.annotations.Test;
 
@@ -14,6 +15,9 @@ public class POMPractices {
 
         //3- Do not enter any information
         //4- Click to “Sign in” button
+        LibraryLoginPage libraryLoginPage=new LibraryLoginPage();
+        libraryLoginPage.
+
         //5- Verify expected error is displayed:
         //Expected: This field is required.
 
