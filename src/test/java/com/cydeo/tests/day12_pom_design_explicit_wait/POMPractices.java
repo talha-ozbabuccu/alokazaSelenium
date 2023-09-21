3fd8c923d6486aba03fb75ea243a442260b519b9
Talha Özbabuccu <talhaoz105@gmail.com>
2023-09-21 16:00:24 +0300

    day12 LibraryLoginPage.java

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -2,10 +2,12 @@ package com.cydeo.tests.day12_pom_design_explicit_wait;
 
 import com.cydeo.pages.LibraryLoginPage;
 import com.cydeo.utilities.Driver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class POMPractices {
 
+    LibraryLoginPage libraryLoginPage;
     @Test
     public void required_field_error_message_test(){
         //TC #1: Required field error message test
@@ -15,11 +17,28 @@ public class POMPractices {
 
         //3- Do not enter any information
         //4- Click to “Sign in” button
-        LibraryLoginPage libraryLoginPage=new LibraryLoginPage();
-        libraryLoginPage.
+        libraryLoginPage=new LibraryLoginPage();
+        libraryLoginPage.signInButton.click();
 
         //5- Verify expected error is displayed:
         //Expected: This field is required.
+        Assert.assertTrue(libraryLoginPage.fieldRequiredErrorMessage.isDisplayed());
+        Driver.closeDriver();
+    }
+
+    @Test
+    public void invalid_email_format_error_message_test(){
+        //TC #2: Invalid email format error message test
+        //1- Open a chrome browser
+        //2- Go to: https://library1.cydeo.com
+        Driver.getDriver().get("https://library1.cydeo.com");
+
+        //3- Enter invalid email format
+        libraryLoginPage.inputUsername.sendKeys("sthwrong");
+
+
+        //4- Verify expected error is displayed:
+        //Expected: Please enter a valid email address.
 
 
     }
