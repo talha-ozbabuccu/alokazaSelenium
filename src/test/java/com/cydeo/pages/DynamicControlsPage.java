95810150a58d37b6f9a931ea7408eb6c56fd829c
Talha Özbabuccu <talhaoz105@gmail.com>
2023-10-01 14:12:29 +0300

    day13 DynamicControlsPage.java

---
+++ src/test/java/com/cydeo/pages/DynamicControlsPage.java
@@ -0,0 +1,22 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DynamicControlsPage {
+
+    public DynamicControlsPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    //FindBy(xpath= "//button[.='Remove']
+    @FindBy(css = "form#checkboc-example > button")
+    public WebElement removeButton;
+    @FindBy(css = "div#loading")
+    public WebElement loadingBar;
+    @FindBy(css = "input[label='blah']")
+    public WebElement checkbox;
+    @FindBy(css="p[id='message']")
+    public WebElement message;
+}
