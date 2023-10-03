2ae3d48da28c360472e6c4a03c32a5ecd71fde23
Talha Özbabuccu <talhaoz105@gmail.com>
2023-10-03 16:06:36 +0300

    day13 ExplicitWaitPractices.java

--- src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
@@ -45,4 +45,15 @@ public class ExplicitWaitPractices {
         Assert.assertTrue(dynamicControlsPage.message.isDisplayed());
         Assert.assertTrue(dynamicControlsPage.message.getText().equals("It's gone!"));
     }
+
+    @Test
+    public void enable_button_test(){
+        //1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/dynamic_controls
+        //3- Click to “Enable” button
+        //4- Wait until “loading bar disappears”
+        //5- Verify:
+        //a. Input box is enabled.
+        //b. “It’s enabled!” message is displayed.
+    }
 }
